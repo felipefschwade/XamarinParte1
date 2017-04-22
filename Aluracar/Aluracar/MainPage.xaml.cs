0d52153eefac96c20c29c78696e5cc34d6e39459
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-21 22:58:17 -0400

    Criando a classe Carro. Realizando um Binding da lista de veiculos da classe e da view. Exibindo os veiculos da lista cadastrada.

--- Aluracar/Aluracar/MainPage.xaml.cs
+++ Aluracar/Aluracar/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aluracar.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,18 @@ namespace Aluracar
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        public IList<Carro> Veiculos { get; set; }
+
+        public MainPage()
 		{
 			InitializeComponent();
-		}
+            Veiculos = new List<Carro>()
+            {
+                new Carro { Nome = "Azera", Valor = 58000 },
+                new Carro { Nome = "Fiesta 2.0", Valor = 40000 },
+                new Carro { Nome = "S10 Flex", Valor = 30000 }
+            };
+            BindingContext = this;
+        }
 	}
 }
---
+++ Aluracar/Aluracar/Model/Carro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aluracar.Model
+{
+    public class Carro
+    {
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
