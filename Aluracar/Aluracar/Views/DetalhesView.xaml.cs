0d895010d29cb98c1f7a61bbe63e8b31872bd4e5
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-28 21:17:03 -0400

    Adicionando a tela de configuração de opcionais.

--- Aluracar/Aluracar/Views/DetalhesView.xaml.cs
+++ Aluracar/Aluracar/Views/DetalhesView.xaml.cs
@@ -14,6 +14,69 @@ namespace Aluracar.Views
 	public partial class DetalhesView : ContentPage
 	{
         public Carro Veiculo { get; set; }
+        const int ABS = 800;
+        const int AR_COND = 1000;
+        const int MP3 = 500;
+
+        bool temAbs;
+        public bool TemAbs
+        {
+            get { return temAbs; }
+            set
+            {
+                temAbs = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        bool temAr;
+        public bool TemAr
+        { 
+            get { return temAr; }
+            set
+            {
+                temAr = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        bool temMp3;
+        public bool TemMp3
+        {
+            get { return temMp3; }
+            set
+            {
+                temAbs = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string TextoAbs
+        {
+            get { return String.Format("Freio ABS - R$ {0}", ABS); }
+        }
+        public string TextoArCond
+        {
+            get { return String.Format("Ar Condicionado - R$ {0}", AR_COND); }
+        }
+        public string TextoMp3
+        {
+            get { return String.Format("Ar Condicionado - R$ {0}", MP3); }
+        }
+
+        public string ValorTotal
+        {
+            get
+            {
+                return String.Format("Valor Total R$ {0}",
+              this.Veiculo.Valor + (this.TemAbs ? ABS : 0)
+              + (this.TemAr ? AR_COND : 0)
+              + (this.TemMp3 ? MP3 : 0)
+              );
+            }
+        }
+
         public DetalhesView (Carro veiculo)
 		{
 			InitializeComponent ();
